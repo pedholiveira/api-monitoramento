bd0cb5b5107d8c02610efd885a1943a6e80322cc
pedholiveira <ped.holiveira@gmail.com>
2018-11-16 09:30:06 -0200

    Inclusão de rotas novas

--- config/database.js
+++
@@ -1,2 +0,0 @@
-const mongoose = require('mongoose')
-module.exports = mongoose.connect('mongodb://api:api123@ds223542.mlab.com:23542/monitoramento')
--- config/routes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express')
-
-module.exports = function(server) {
-    const router = express.Router()
-    server.use('/api', router)
-
-    const consumoService = require('../itens/consumoService')
-    consumoService.register(router, '/consumo')
-}
