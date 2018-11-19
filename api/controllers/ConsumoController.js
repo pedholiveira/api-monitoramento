2587bca80ce17042ec7852f835c2658817b3327c
pedro.silva <pedro.silva@radixeng.com.br>
2018-11-19 14:51:00 -0200

    Removido json stringify

--- api/controllers/ConsumoController.js
+++ api/controllers/ConsumoController.js
@@ -12,7 +12,7 @@ exports.obterConsumos = function (req, res) {
         if (err) {
             res.send(err)
         }            
-        res.json(JSON.stringify(agruparConsumos(consumos)))
+        res.json(agruparConsumos(consumos))
     })
 }
 
